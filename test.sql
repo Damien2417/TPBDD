02fb02278aaa1fd6c7473bfb47abad55561c069a
Damien Bukudjian <damienbuk@live.fr>
2022-05-23 18:02:55 +0200

    add db

--- test.sql
+++ test.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.2
+-- version 5.1.1
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le : ven. 13 mai 2022 à 09:12
--- Version du serveur :  8.0.21
--- Version de PHP : 7.3.21
+-- Généré le : lun. 23 mai 2022 à 15:59
+-- Version du serveur : 5.7.36
+-- Version de PHP : 7.4.26
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,76 +24,31 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Structure de la table `acces`
---
-
-DROP TABLE IF EXISTS `acces`;
-CREATE TABLE IF NOT EXISTS `acces` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `prenom` varchar(20) NOT NULL,
-  `login` varchar(20) NOT NULL,
-  `password` varchar(20) NOT NULL,
-  `statut` varchar(20) NOT NULL,
-  `age` int NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
-
---
--- Déchargement des données de la table `acces`
---
-
-INSERT INTO `acces` (`id`, `prenom`, `login`, `password`, `statut`, `age`) VALUES
-(1, 'Tom', 'tomahawk', 'indien', 'Etudiant', 22),
-(2, 'Pierre', 'a', 'a', 'Prof', 44),
-(3, 'Michel', 'lamere', 'sonchat', 'Admin', 69),
-(4, 'Robin', 'Locksley', 'desbois', 'Etudiant', 23);
-
--- --------------------------------------------------------
-
---
 -- Structure de la table `creneaux`
 --
 
 DROP TABLE IF EXISTS `creneaux`;
 CREATE TABLE IF NOT EXISTS `creneaux` (
-  `id` int NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `date` date NOT NULL,
   `nom` varchar(30) NOT NULL,
   `heure` float NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=9 DEFAULT CHARSET=latin1;
 
 --
 -- Déchargement des données de la table `creneaux`
 --
 
 INSERT INTO `creneaux` (`id`, `date`, `nom`, `heure`) VALUES
-(1, '2022-05-11', 'test', 10),
-(2, '2022-05-10', 'test3', 12.5),
-(3, '2022-05-10', 'test2', 10.5);
-
--- --------------------------------------------------------
-
---
--- Structure de la table `statut`
---
-
-DROP TABLE IF EXISTS `statut`;
-CREATE TABLE IF NOT EXISTS `statut` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `nom` varchar(20) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
-
---
--- Déchargement des données de la table `statut`
---
-
-INSERT INTO `statut` (`id`, `nom`) VALUES
-(1, 'Etudiant'),
-(2, 'Prof'),
-(3, 'Admin'),
-(4, 'Visiteur');
+(1, '2022-05-11', 'Tom', 10),
+(2, '2022-05-10', 'Michel', 12.5),
+(3, '2022-05-10', 'Robin', 10.5),
+(4, '2022-05-23', 'Tom', 16),
+(5, '2022-05-27', 'Michel', 15.5),
+(6, '2022-05-25', 'Robin', 9),
+(7, '2022-05-25', 'Pierre', 10.5),
+(8, '2022-05-24', 'Pierre', 13.5);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
