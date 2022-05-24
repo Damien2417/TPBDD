b4fcce65f96e0a5903e27b7071a1e1aa5715e3be
Damien Bukudjian <damienbuk@live.fr>
2022-05-24 22:44:20 +0200

    e

--- test.sql
+++ test.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le : lun. 23 mai 2022 à 15:59
+-- Généré le : mar. 24 mai 2022 à 20:43
 -- Version du serveur : 5.7.36
 -- Version de PHP : 7.4.26
 
@@ -20,6 +20,35 @@ SET time_zone = "+00:00";
 --
 -- Base de données : `test`
 --
+CREATE DATABASE IF NOT EXISTS `test` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
+USE `test`;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `acces`
+--
+
+DROP TABLE IF EXISTS `acces`;
+CREATE TABLE IF NOT EXISTS `acces` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `prenom` varchar(20) NOT NULL,
+  `login` varchar(20) NOT NULL,
+  `password` varchar(20) NOT NULL,
+  `statut` varchar(20) NOT NULL,
+  `age` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+
+--
+-- Déchargement des données de la table `acces`
+--
+
+INSERT INTO `acces` (`id`, `prenom`, `login`, `password`, `statut`, `age`) VALUES
+(1, 'Tom', 'tomahawk', 'indien', 'Patient', 22),
+(2, 'Pierre', 'a', 'a', 'Medecin', 44),
+(3, 'Michel', 'lamere', 'sonchat', 'Patient', 69),
+(4, 'Robin', 'Locksley', 'desbois', 'Patient', 23);
 
 -- --------------------------------------------------------
 
@@ -34,7 +63,7 @@ CREATE TABLE IF NOT EXISTS `creneaux` (
   `nom` varchar(30) NOT NULL,
   `heure` float NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=9 DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=12 DEFAULT CHARSET=latin1;
 
 --
 -- Déchargement des données de la table `creneaux`
@@ -48,7 +77,10 @@ INSERT INTO `creneaux` (`id`, `date`, `nom`, `heure`) VALUES
 (5, '2022-05-27', 'Michel', 15.5),
 (6, '2022-05-25', 'Robin', 9),
 (7, '2022-05-25', 'Pierre', 10.5),
-(8, '2022-05-24', 'Pierre', 13.5);
+(8, '2022-05-24', 'Pierre', 13.5),
+(9, '2022-05-27', 'Pierre', 17),
+(10, '2022-05-17', 'Pierre', 11),
+(11, '2022-05-19', 'Pierre', 12.5);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
